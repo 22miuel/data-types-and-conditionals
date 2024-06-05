a510a92032de21266bd176dadc0582a0349796eb
22miuel <22miguel.gallardo22@gmail.com>
2024-06-05 16:42:52 -0400

    end some exercises for oddin with javascript

---
+++ exercises of js/if-else.js
@@ -0,0 +1,42 @@
+/*1*/
+
+if ("0") {
+    alert( 'Hello' );
+  }
+/*si*/
+
+
+
+/*2*/
+let value = prompt('Type a number', 0);
+
+if (value > 0) {
+  alert( 1 );
+} else if (value < 0) {
+  alert( -1 );
+} else {
+  alert( 0 );
+}
+
+
+
+/*3*/
+
+let result = (a + b < 4)? 'Below' : 'Over';
+
+
+/*4*/
+
+let mensagge = (login == 'Employee') ? 'Hello', (login == 'Director') ? 'Greetings'. (login == 'No login') ? '';
+
+
+/* 5 */
+
+
+let value = prompt('What is the "official" name of JavaScript?', '');
+
+if (value ==  “ECMAScript” ) {
+    alert ('Right')
+}else {
+    alert('Dont know? ¡ECMAScript!”')
+}
